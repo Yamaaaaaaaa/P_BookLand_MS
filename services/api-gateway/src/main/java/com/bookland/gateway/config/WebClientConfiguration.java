3b8b438eeba75c391c8a9b1b7d29b4029dec7376
Tran Xuan Son <110320455+Yamaaaaaaaa@users.noreply.github.com>
2026-05-26 16:42:16 +0700

    Fix CORS global

--- services/api-gateway/src/main/java/com/bookland/gateway/config/WebClientConfiguration.java
+++ services/api-gateway/src/main/java/com/bookland/gateway/config/WebClientConfiguration.java
@@ -5,9 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -28,18 +25,6 @@ public class WebClientConfiguration {
                 .build();
     }
 
-    @Bean
-    CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("*"));
-        corsConfiguration.setAllowedHeaders(List.of("*"));
-        corsConfiguration.setAllowedMethods(List.of("*"));
-
-        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-
-        return new CorsWebFilter(urlBasedCorsConfigurationSource);
-    }
 
     @Bean
     public KeyResolver userKeyResolver() {
