532bb865f97adf9d6df7075c7d5467fd84babeee
Tran Xuan Son <110320455+Yamaaaaaaaa@users.noreply.github.com>
2026-05-27 12:06:44 +0700

    z

--- services/order-service/src/main/java/com/bookland/order/config/SecurityConfig.java
+++ services/order-service/src/main/java/com/bookland/order/config/SecurityConfig.java
@@ -22,6 +22,7 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig {
 
+
     private static final String[] PUBLIC_PATHS = {
             "/api/payment-methods/**",
             "/api/shipping-methods/**",
